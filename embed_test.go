c2333636bcce4f9908b893e2b880a328ef612ff3
adityaputra42 <aditiyaputra42@gmail.com>
2023-08-30 14:08:28 +0700

    first init

---
+++ embed_test.go
@@ -0,0 +1,45 @@
+package golangembed
+
+import (
+	"embed"
+	_ "embed"
+	"fmt"
+	"io/fs"
+	"os"
+	"testing"
+)
+
+//go:embed version.txt
+var version string
+
+func TestString(t *testing.T) {
+
+	fmt.Println(version)
+
+}
+
+//go:embed wallpaper.jpg
+var wallpaper []byte
+
+func TestByteArray(t *testing.T) {
+	err := os.WriteFile("wallpaper_next.jpg", wallpaper, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+}
+
+//go:embed files/a.txt
+//go:embed files/b.txt
+//go:embed files/c.txt
+var files embed.FS
+
+func TestMultiFile(t *testing.T) {
+	a, _ := files.ReadFile("files/a.txt")
+	fmt.Println(string(a))
+
+	b, _ := files.ReadFile("files/b.txt")
+	fmt.Println(string(b))
+
+	c, _ := files.ReadFile("files/c.txt")
+	fmt.Println(string(c))
+}
